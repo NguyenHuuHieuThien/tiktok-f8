defab2bcfefc70e6a98712258d36f23f34ae2d87
NguyenHuuHieuThien <78004234+NguyenHuuHieuThien@users.noreply.github.com>
2023-02-14 23:52:16 +0700

    Xây dựng Logic phần Header #2

--- src/Component/AccountItem/index.js
+++ src/Component/AccountItem/index.js
@@ -1,29 +1,29 @@
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faCircleCheck } from '@fortawesome/free-solid-svg-icons';
-import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 import Image from '../Images';
 
 const cx = classNames.bind(styles);
 
-function AccountItem() {
+function AccountItem({ data }) {
   return (
-    <div className={cx('wrapper')}>
+    <Link to={'/@' + data.nickname} className={cx('wrapper')}>
       <Image
-        src="https://vietnamnexttopmodel.com/wp-content/uploads/2022/04/phim-cua-hua-khai.jpg"
         className={cx('avatar')}
+        src={data.avatar}
         alt=""
         fallback="https://static2.yan.vn/YanNews/2167221/202006/cuc-tinh-y-la-ai-tieu-su-su-nghiep-doi-tu-cua-nu-dien-vien-ca-si-16d498dc.jpeg"
       />
       <div className={cx('info')}>
         <div className={cx('name')}>
-          <span>Nguyen Huu Hieu Thien</span>
-          <FontAwesomeIcon className={cx('icon')} icon={faCircleCheck} />
+          <span>{data.full_name}</span>
+          {data.tick && <FontAwesomeIcon className={cx('icon')} icon={faCircleCheck} />}
         </div>
-        <div className={cx('username')}>hieuthien1770</div>
+        <div className={cx('username')}>{data.nickname}</div>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/Component/Layout/Component/Search/index.js
+++ src/Component/Layout/Component/Search/index.js
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import HeadLessTippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'tippy.js/dist/tippy.css';
+import axios from 'axios';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 import { Wrapper as PopperWrapper } from '~/Component/Popper';
@@ -14,14 +15,25 @@ function Search() {
   const [searchResult, setSearchResult] = useState([]);
   const [searchValue, setSearchValue] = useState('');
   const [showResult, setShowResult] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const inputRef = useRef();
 
   useEffect(() => {
+    if (!searchValue.trim()) {
+      return;
+    }
+    setLoading(true);
     setTimeout(() => {
-      setSearchResult([1, 2, 3]);
-    }, 3000);
-  }, []);
+      axios
+        .get(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        .then((res) => {
+          setSearchResult(res.data.data);
+          setLoading(false);
+        })
+        .catch((err) => setLoading(false));
+    }, 1000);
+  }, [searchValue]);
 
   const handleHideResult = () => {
     setShowResult(false);
@@ -32,6 +44,7 @@ function Search() {
     setSearchResult([]);
     inputRef.current.focus();
   };
+  console.log(searchResult);
   return (
     <HeadLessTippy
       visible={searchResult.length > 0 && showResult}
@@ -41,11 +54,9 @@ function Search() {
         <div className={cx('search-result')} tabIndex="-1" {...attrs}>
           <PopperWrapper>
             <h4 className={cx('search-title')}>Accounts</h4>
-            <AccountItem />
-            <AccountItem />
-            <AccountItem />
-            <AccountItem />
-            <AccountItem />
+            {searchResult.map((item) => (
+              <AccountItem key={item.id} data={item} />
+            ))}
           </PopperWrapper>
         </div>
       )}
@@ -59,12 +70,12 @@ function Search() {
           onChange={(e) => setSearchValue(e.target.value)}
           spellCheck={false}
         />
-        {!!searchValue && (
+        {!!searchValue && !loading && (
           <button className={cx('clear')} onClick={handleClear}>
             <FontAwesomeIcon icon={faCircleXmark} />
           </button>
         )}
-        {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+        {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
         <button className={cx('search-btn')}>
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
